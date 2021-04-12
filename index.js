1b938f7df2a36e7b9dd128d8946278fa64b19fb0
Hash52 <hirokinoakaunto@gmail.com>
2021-04-12 19:43:34 +0900

    hello component

--- index.js
+++ index.js
@@ -50,3 +50,23 @@ var app6 = new Vue({
       message: 'Hello Vue!'
     }
 })
+
+// todo-item と呼ばれる新しいコンポーネントを定義
+Vue.component('todo-item', {
+    // todo-item コンポーネントはカスタム属性のような "プロパティ" で受け取ります。
+    // このプロパティは todo と呼ばれます。
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+    el: '#app-7',
+    data: {
+      groceryList: [
+        { id: 0, text: 'Vegetables' },
+        { id: 1, text: 'Cheese' },
+        { id: 2, text: 'Whatever else humans are supposed to eat' }
+      ]
+    }
+})
+    
