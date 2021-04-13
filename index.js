f09807bba9dfe29a91df16fb26091594b601a708
Hash52 <hirokinoakaunto@gmail.com>
2021-04-13 14:28:18 +0900

    インスタンスとライフサイクルフックについて学習 https://jp.vuejs.org/v2/guide/instance.html

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
+var obj = {message: "this message is fixed"}
+
+//dataに渡してもリアクティブシステムが追跡できなくなる唯一の例外
+Object.freeze(obj)
+
 var app = new Vue({ 
     el: '#app',
+    //dataに渡したオブジェクトの全てのプロパティをリアクティブシステムに追加
     data: {
-        message: 'Hello test2!!',
-        message2: 'Hello Vue2!!'
+        message: 'Hello hello!!',
+        message2: 'Hello Vue2!!',
+        message4: null, //app.message4はリアクティブ
+        obj: obj
+    },
+    created: function(){
+      console.log("createはライフサイクルフックの一種で、インスタンスが生成された後に実行する処理を記述できる")
+      //thisはappインスタンスを返す
+      console.log('app.message は ' + this.messsage)
+      /* ライフサイクルフックは他にも、mounted、updated、destroyedなどがある */
     }
+    /*(注意) コールバックでアロー関数を使用しないこと
+    例　created: () => console.log(this.message)
+    アロー関数は this をもたないため、this は他の変数と同様に見つかるまで親スコープをレキシカルに探索されエラー
+    */
 });
+
+//インスタンス作成時に存在していたdataのみリアクティブシステムは有効
+app.message3 = "not reactive"
+
+/*Vueインスタンスは、いくつかの便利なプロパティとメソッドを持っており、
+これらはユーザ定義のプロパティと区別するために、頭に $ が付けられている
+https://jp.vuejs.org/v2/api/#インスタンスプロパティ */
+
+app.$data //appのリアクティブなプロパティ一覧を取得
+app.$el === document.getElementById('app') // => true
+
+// $watch はインスタンスメソッド
+app.$watch('message2', function (newValue, oldValue) {
+  console.log('このコールバックは `app.message2` の値が変わる時に呼ばれます')
+})
 
 var app2 = new Vue({
     el: '#app-2',
